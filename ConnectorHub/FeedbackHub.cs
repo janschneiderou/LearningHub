90122c6fa69bdbf09221c2f9dfc7b552bf7bfa9f
Jan Schneider <jan.schneiderbarnes@gmail.com>
2018-06-05 11:56:10 +0200

    added copyright

--- ConnectorHub/FeedbackHub.cs
+++ ConnectorHub/FeedbackHub.cs
@@ -1,4 +1,24 @@
-﻿using System;
+﻿/**
+ * ****************************************************************************
+ * Copyright (C) 2018 Das Deutsche Institut für Internationale Pädagogische Forschung (DIPF)
+ * <p/>
+ * This library is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * <p/>
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ * <p/>
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this library.  If not, see <http://www.gnu.org/licenses/>.
+ * <p/>
+ * Contributors: Jan Schneider
+ * ****************************************************************************
+ */
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
